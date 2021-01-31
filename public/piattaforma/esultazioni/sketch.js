516e1f4303f0acf116586ab84a3221222a997569
JessicaMoreschi <jessica.moreschi98@gmail.com>
2021-01-31 14:51:21 +0100

    p

--- public/piattaforma/esultazioni/sketch.js
+++ public/piattaforma/esultazioni/sketch.js
@@ -78,7 +78,7 @@ function bonus_server(data) {
 ////////////////FINE COMUNICAZIONE SERVER/////////////////////////////////////
 
 function preload() {
-  baloonIcon = loadImage("./assets/feed_parla.gif");  //nuvoletta attiva
+  baloonIcon = loadImage("./assets/fParla.gif");  //nuvoletta attiva
   baloon_Puntini = loadImage("./assets/scuro.gif"); //nuvoletta pensa
   noParola = loadImage("./assets/noParola.png"); //nuvoletta attiva
   logor = loadImage("./assets/logopiccolo.png") //logo ridotto
